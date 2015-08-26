8b1442f30edf460664bf22e887bda3fc21247f71
Hasitha Liyanage <hasitha@thinkcube.com>
2015-08-26 14:56:05 +0530

    store working with dispatch calls (no action creators)

--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import App from './App';
 import Store from '../lib/Store';
+import { SampleStore  } from '../lib/SampleStore';
 
+Store.register('samples', SampleStore, null);
+Store.subscribe('samples', 'change', function() {
+    console.log('samples changed');
+});
+Store.dispatch({ actionType: 'createSample', text: 'Sample'});
 
 React.render(<App />, document.getElementById('app'));
