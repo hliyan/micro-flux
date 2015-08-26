793b58ffbb469a6db6468e75841d95700487157e
Hasitha Liyanage <hasitha@thinkcube.com>
2015-08-26 18:23:08 +0530

    working dispatcher sample

--- app/index.js
+++ app/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import App from './App';
-import Dispatcher from '../lib/Dispatcher';
-import { SampleStore  } from '../lib/SampleStore';
-
-Dispatcher.register('samples', SampleStore, null);
-Dispatcher.subscribe('samples', 'change', function() {
-    console.log('samples changed');
-});
-Dispatcher.dispatch({ actionType: 'createSample', text: 'Sample'});
+import Sample from '../lib/Sample';
 
 React.render(<App />, document.getElementById('app'));
+
+Sample.run();
