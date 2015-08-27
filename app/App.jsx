1703363cb86674d7585351adb5540ebb09486931
Hasitha Liyanage <hasitha@thinkcube.com>
2015-08-27 20:36:23 +0530

    sample app - refactored noteindex

--- app/App.jsx
+++ app/App.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import Dispatcher from './Dispatcher';
-import NoteList from './notes/NoteList';
-import NoteEntry from './notes/NoteEntry';
+import NoteIndex from './NoteIndex';
 
 export default class App extends React.Component {  
     constructor(props) {
         super(props);
         this.state = {};
-        this.state.notes = Dispatcher.get('notes');
-        Dispatcher.dispatch({ action: 'createNote', data: { key: 1, text: 'Sample Note'} });
     }
 
     render() {
         return (
             <div>
-                <h1>Hello</h1>
-                <NoteEntry />
-                <NoteList notes={this.state.notes} />
+                <NoteIndex />
             </div>
         );
     }
