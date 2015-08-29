18c251fa854d0a069cbe972da279bda1fa6e069f
Hasitha Liyanage <hasitha@thinkcube.com>
2015-08-29 13:57:31 +0530

    refactored stores to remove switch, started on api

--- app/notes/NoteStore.js
+++ app/notes/NoteStore.js
@@ -1,11 +1,28 @@
-import BasicStore from '../../lib/BasicStore';
+import ArrayStore from '../../lib/ArrayStore';
+import RESTApi from '../../lib/RESTApi';
 
-export default class NoteStore extends BasicStore {
-    onAction(action) {
-        switch(action.action) {
-            case 'createNote':
-                this.create(action.data);
-            break;
-        }   
+export default class NoteStore extends ArrayStore {
+    constructor() {
+        super();
+        this.api = new RESTApi('api.foo.com');
+    }
+
+    createNote(action) {
+        this.data.push(action.data);
+        this.emit('change');
+    }
+
+    downloadNotes(action) {
+        this.api.get('/notes').then((res) => {
+            if (res.code == 200) {
+                this.data = res.body.data;
+                this.emit('change');
+            } else {
+                this.emit('error');
+            }
+        }).catch((err) => {
+            this.emit('error');
+        });
+        this.emit('pending_change');       
     }
 }
